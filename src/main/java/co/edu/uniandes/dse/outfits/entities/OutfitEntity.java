0e2d1a3ea21a4a5e3d8b5e845035d7bcd798a2e8
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-02-12 18:24:52 -0500

    Add entity Outfit

---
+++ src/main/java/co/edu/uniandes/dse/outfits/entities/OutfitEntity.java
@@ -0,0 +1,18 @@
+package co.edu.uniandes.dse.outfits.entities;
+
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class OutfitEntity extends BaseEntity {
+    private String ad;
+}
+
+
+
+
+
+
