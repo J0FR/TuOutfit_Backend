0262f86d20a2c1c67d06b461c8b3ee5ef690e67f
Daniel Pedroza <d.pedroza@uniandes.edu.co>
2023-02-20 19:04:06 -0500

    Correcciones

--- src/main/java/co/edu/uniandes/dse/outfits/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/outfits/exceptions/ErrorMessage.java
@@ -2,12 +2,13 @@ package co.edu.uniandes.dse.outfits.exceptions;
 
 public final class ErrorMessage {
 
-    public static final String UBICACION_NOT_FOUND = "La ubicacion no existe";
-    public static final String OUTFIT_NOT_FOUND = "El outfit no existe";
-    public static final String TIENDA_FISICA_NOT_FOUND = "La ubicacion no existe";
-    public static final String USUARIO_NOT_FOUND = "El usuario no existe";
-    
-    private ErrorMessage() {
-		throw new IllegalStateException("Utility class");
+  public static final String UBICACION_NOT_FOUND = "La ubicacion no existe";
+  public static final String OUTFIT_NOT_FOUND = "El outfit no existe";
+  public static final String TIENDA_FISICA_NOT_FOUND = "La ubicacion no existe";
+  public static final String USUARIO_NOT_FOUND = "El usuario no existe";
+
+  private ErrorMessage() {
+    throw new IllegalStateException("Utility class");
+  }
 
 }
