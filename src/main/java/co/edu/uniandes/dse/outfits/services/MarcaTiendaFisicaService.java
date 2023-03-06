9d47a633b816cf4b838bf45094e1e8657c5b2d5d
Daniel Pedroza <d.pedroza@uniandes.edu.co>
2023-03-05 19:38:19 -0500

    Arreglos de coverage

--- src/main/java/co/edu/uniandes/dse/outfits/services/MarcaTiendaFisicaService.java
+++ src/main/java/co/edu/uniandes/dse/outfits/services/MarcaTiendaFisicaService.java
@@ -58,7 +58,7 @@ public class MarcaTiendaFisicaService {
 
         List<TiendaFisicaEntity> tiendaFisicaEntities = marcaEntity.get().getTiendas_fisicas();
 
-        if (tiendaFisicaEntities.size() == 0)
+        if (tiendaFisicaEntities.isEmpty())
             throw new EntityNotFoundException(ErrorMessage.TIENDA_FISICA_NOT_FOUND);
 
         log.info("Finaliza proceso de consultar todas las tiendas fisicas de la marca con id = {0}", marcaId);
