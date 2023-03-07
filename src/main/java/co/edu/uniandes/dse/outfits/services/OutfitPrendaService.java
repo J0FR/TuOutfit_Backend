ee16555ff58c7383aececa3afbd8e2774e17986b
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2023-03-06 23:15:29 -0500

    Cambios Menores

--- src/main/java/co/edu/uniandes/dse/outfits/services/OutfitPrendaService.java
+++ src/main/java/co/edu/uniandes/dse/outfits/services/OutfitPrendaService.java
@@ -9,6 +9,7 @@ import co.edu.uniandes.dse.outfits.entities.OutfitEntity;
 import co.edu.uniandes.dse.outfits.entities.PrendaEntity;
 import co.edu.uniandes.dse.outfits.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.outfits.exceptions.ErrorMessage;
+import co.edu.uniandes.dse.outfits.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.outfits.repositories.OutfitRepository;
 import co.edu.uniandes.dse.outfits.repositories.PrendaRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -16,65 +17,123 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class OutfitPrendaService {
-    @Autowired
+	@Autowired
 	private OutfitRepository outfitRepository;
 
 	@Autowired
 	private PrendaRepository prendaRepository;
-
-
+	
+	/**
+	 * Asocia un Prenda existente a un Outfit
+	 *
+	 * @param outfitId 
+	 * @param prendaId 
+	 * @return Instancia de PrendaEntity que fue asociada a Outfit
+	 */
 	@Transactional
 	public PrendaEntity addPrenda(Long outfitId, Long prendaId) throws EntityNotFoundException {
-		log.info("Inicia proceso de asociar el outfit con id = {0} al usuario con id = " + outfitId, prendaId);
+		log.info("Inicia proceso de asociarle un Prenda al outfit con id = {0}", outfitId);
+		Optional<PrendaEntity> prendaEntity = prendaRepository.findById(prendaId);
+		if (prendaEntity.isEmpty())
+			throw new EntityNotFoundException(ErrorMessage.PRENDA_NOT_FOUND);
+
 		Optional<OutfitEntity> outfitEntity = outfitRepository.findById(outfitId);
 		if (outfitEntity.isEmpty())
 			throw new EntityNotFoundException(ErrorMessage.OUTFIT_NOT_FOUND);
 
-		Optional<PrendaEntity> prendaEntity = prendaRepository.findById(prendaId);
-		if (prendaEntity.isEmpty())
-			throw new EntityNotFoundException(ErrorMessage.USUARIO_NOT_FOUND);
+		outfitEntity.get().getPrendas().add(prendaEntity.get());
+		log.info("Termina proceso de asociarle un Prenda al outfit con id = {0}", outfitId);
+		return prendaEntity.get();
+	}
 
-		outfitEntity.get().addPrenda(prendaEntity.get());
-		log.info("Termina proceso de asociar el outfit con id = {0} al usuario con id = {1}", outfitId, prendaId);
+	/**
+	 * Obtiene una colección de instancias de PrendaEntity asociadas a una instancia de Outfit
+	 *
+	 * @param outfitId 
+	 * @return Colección de instancias de PrendaEntity asociadas a la instancia de Outfit
+	 */
+	@Transactional
+	public List<PrendaEntity> getPrendas(Long outfitId) throws EntityNotFoundException {
+		log.info("Inicia proceso de consultar todos los Prendas del outfit con id = {0}", outfitId);
+		Optional<OutfitEntity> outfitEntity = outfitRepository.findById(outfitId);
+		if (outfitEntity.isEmpty())
+			throw new EntityNotFoundException(ErrorMessage.OUTFIT_NOT_FOUND);
+		log.info("Finaliza proceso de consultar todos los Prendas del outfit con id = {0}", outfitId);
+		return outfitEntity.get().getPrendas();
+	}
+
+	/**
+	 * Obtiene una instancia de PrendaEntity asociada a una instancia de Outfit
+	 *
+	 * @param outfitId 
+	 * @param PrendaId 
+	 * @return La entidad del Prenda asociada al Outfit
+	 */
+	@Transactional
+	public PrendaEntity getPrenda(Long outfitId, Long prendaId)
+			throws EntityNotFoundException, IllegalOperationException {
+		log.info("Inicia proceso de consultar un Prenda del outfit con id = {0}", outfitId);
+		Optional<PrendaEntity> prendaEntity = prendaRepository.findById(prendaId);
+		Optional<OutfitEntity> outfitEntity = outfitRepository.findById(outfitId);
+
+		if (prendaEntity.isEmpty())
+			throw new EntityNotFoundException(ErrorMessage.PRENDA_NOT_FOUND);
+
+		if (outfitEntity.isEmpty())
+			throw new EntityNotFoundException(ErrorMessage.OUTFIT_NOT_FOUND);
+		log.info("Termina proceso de consultar un Prenda del outfit con id = {0}", outfitId);
+		if (!outfitEntity.get().getPrendas().contains(prendaEntity.get()))
+			throw new IllegalOperationException("The Prenda is not associated to the outfit");
+		
 		return prendaEntity.get();
 	}
 
 	@Transactional
-	public List<PrendaEntity> getPrendas(Long outfitId) throws EntityNotFoundException {
-		log.info("Inicia proceso de consultar los usuario del outfit con id = {0}", outfitId);
+	/**
+	 * Remplaza las instancias de Prenda asociadas a una instancia de Outfit
+	 *
+	 * @param outfitId 
+	 * @param listPrendas    
+	 * @return Nueva colección de PrendaEntity asociada a la instancia de Outfit
+	 */
+	public List<PrendaEntity> replacePrendas(Long outfitId, List<PrendaEntity> listPrendas) throws EntityNotFoundException {
+		log.info("Inicia proceso de reemplazar los Prendas del outfit con id = {0}", outfitId);
 		Optional<OutfitEntity> outfitEntity = outfitRepository.findById(outfitId);
 		if (outfitEntity.isEmpty())
 			throw new EntityNotFoundException(ErrorMessage.OUTFIT_NOT_FOUND);
 
-        List<PrendaEntity> prendaEntity = outfitEntity.get().getPrendas();
+		for (PrendaEntity author : listPrendas) {
+			Optional<PrendaEntity> prendaEntity = prendaRepository.findById(author.getId());
+			if (prendaEntity.isEmpty())
+				throw new EntityNotFoundException(ErrorMessage.PRENDA_NOT_FOUND);
 
-		if (prendaEntity.size() == 0)
-			throw new EntityNotFoundException(ErrorMessage.USUARIO_NOT_FOUND);
-
-		log.info("Termina proceso de consultar los usuario del outfit con id = {0}", outfitId);
-		return prendaEntity;
+			if (!outfitEntity.get().getPrendas().contains(prendaEntity.get()))
+				outfitEntity.get().getPrendas().add(prendaEntity.get());
+		}
+		log.info("Termina proceso de reemplazar los Prendas del outfit con id = {0}", outfitId);
+		return getPrendas(outfitId);
 	}
 
 	@Transactional
+	/**
+	 * Desasocia un Prenda existente de un Outfit existente
+	 *
+	 * @param outfitId   
+	 * @param comentarioId 
+	 */
 	public void removePrenda(Long outfitId, Long prendaId) throws EntityNotFoundException {
-		log.info("Inicia proceso de borrar el usuario del outfit con id = {0}", outfitId);
+		log.info("Inicia proceso de borrar un Prenda del outfit con id = {0}", outfitId);
+		Optional<PrendaEntity> prendaEntity = prendaRepository.findById(prendaId);
 		Optional<OutfitEntity> outfitEntity = outfitRepository.findById(outfitId);
+
+		if (prendaEntity.isEmpty())
+			throw new EntityNotFoundException(ErrorMessage.PRENDA_NOT_FOUND);
+
 		if (outfitEntity.isEmpty())
 			throw new EntityNotFoundException(ErrorMessage.OUTFIT_NOT_FOUND);
 
-        List<PrendaEntity> prendaEntity = outfitEntity.get().getPrendas();
-        Optional<PrendaEntity> prendaToFind = prendaRepository.findById(prendaId);
+		outfitEntity.get().getPrendas().remove(prendaEntity.get());
 
-        if (prendaToFind.isEmpty())
-        throw new EntityNotFoundException(ErrorMessage.COMENTARIO_NOT_FOUND);
-
-        if (prendaEntity.contains(prendaToFind.get())){
-            int index = prendaEntity.indexOf(prendaToFind.get());
-            prendaEntity.remove(index);
-        } else {
-            throw new EntityNotFoundException(ErrorMessage.COMENTARIO_NOT_FOUND);
-        }
-
-		log.info("Termina proceso de borrar el usuario del outfit con id = " + prendaId);
+		log.info("Termina proceso de borrar un Prenda del outfit con id = {0}", outfitId);
 	}
 }
